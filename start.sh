46d2d7d691363d08564ae6647a91aad17ea01d70
Ekko <v55713623@gmail.com>
2025-11-12 18:56:57 +0800

    start: remove pip install from start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-set -e  # 有錯誤就中止
-
-echo "🚀 Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
+set -e
 
 echo "✅ Starting Uvicorn server..."
 exec python -m uvicorn main:app --host 0.0.0.0 --port 10000
