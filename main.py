7ef5ad57f5e996a8072ec348b1cf964aca4a61ba
aiwtf <ekkobuds@gmail.com>
2025-11-28 17:44:02 +0800

    Update CORS to allow all origins

--- main.py
+++ main.py
@@ -276,7 +276,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://polytracking.vercel.app", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
